e5e7b1834dae4a31cbf72224116aebea69e6a2fc
Mariano Basti <31198560+marianobasti@users.noreply.github.com>
2022-11-21 23:12:00 +0000

    Image test

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 var data = {
     "EscenarioPrincipal": "Este es el lugar donde va a sonar la musica ue te encanta. Sonido de alta calidad y visuales enormes para explotar tus sentidos",
     "Barra": "Tragos y agua criminalmente cara",
-    "PuestoCripto": "Sentite un lobo de Wall Street perdiendo todos tus ahorros",
+    "PuestoCrypto": "Sentite un lobo de Wall Street perdiendo todos tus ahorros",
     "FoodTrucks": "Comida pa tu gula",
     "Patiodecomidas": "El piso esta sucio. Ew. Sentate en la mesita",
     "VIP": "Sentite mejor por tener mas plata",
