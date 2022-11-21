4eb36f4c0986e5a4005343ebca17c04623fad6f5
Mariano Basti <31198560+marianobasti@users.noreply.github.com>
2022-11-21 15:36:50 +0000

    Data loading

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-{
+var data = {
   "EscenarioPrincipal": "Este es el lugar donde va a sonar la musica ue te encanta. Sonido de alta calidad y visuales enormes para explotar tus sentidos",
   "Barra": "Tragos y birrita y agua",
   "PuestoCripto": "Pa tus necesidades cripto",
