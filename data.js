31b715e7c65dabd4e4548fdfe390144f00c156f6
Mariano Basti <31198560+marianobasti@users.noreply.github.com>
2022-11-21 21:00:57 +0000

    Cleanup

--- data.js
+++ data.js
@@ -1,6 +1,15 @@
 var data = {
   "EscenarioPrincipal": "Este es el lugar donde va a sonar la musica ue te encanta. Sonido de alta calidad y visuales enormes para explotar tus sentidos",
-  "Barra": "Tragos y birrita y agua",
-  "PuestoCripto": "Pa tus necesidades cripto",
-  "FoodTrucks": "Comida pa tu gula"
+  "Barra": "Tragos y agua criminalmente cara",
+  "PuestoCripto": "Sentite un lobo de Wall Street perdiendo todos tus ahorros",
+  "FoodTrucks": "Comida pa tu gula",
+  "Patiodecomidas": "El piso esta sucio. Ew. Sentate en la mesita",
+  "VIP": "Sentite mejor por tener mas plata",
+  "PuestoEcovaso": "Plastico del que no nos hacemos cargo",
+  "Puestodecarga": "Porque saliste de casa con la bateria al 30%",
+  "Puestosdiseño": "???????????????????",
+  "Totem": "No te vayas a perder",
+  "Acreditaciones": "Duh",
+  "Puestodeprimerosauxilios": "Por si te zarpas en sustancia",
+  "PatioKids": "Para que los trajiste?"
 }
